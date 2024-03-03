a76cb3bbe7ec83151f00986f3ac1a63b0bbc181d
Fernando005-SSF <sadeepfernando12345@gmail.com>
2024-03-03 12:07:55 +0530

    Created a user endpoint with postman

---
+++ sadeep/src/main/java/com/practice/sadeep/Controller/UserController.java
@@ -0,0 +1,22 @@
+package com.practice.sadeep.Controller;
+
+import com.practice.sadeep.Model.User;
+import com.practice.sadeep.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class UserController extends User {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser){
+        return userRepository.save(newUser);
+    }
+
+}
