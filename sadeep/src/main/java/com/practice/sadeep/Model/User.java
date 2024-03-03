7d12d5df7639f879970c44b1690c7ca44ac95d6a
Fernando005-SSF <sadeepfernando12345@gmail.com>
2024-03-03 11:04:57 +0530

    Created the user class and extend it to jpa repository

---
+++ sadeep/src/main/java/com/practice/sadeep/Model/User.java
@@ -0,0 +1,50 @@
+package com.practice.sadeep.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String userName;
+    private String name;
+    private String email;
+
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
---
+++ sadeep/src/main/java/com/practice/sadeep/SadeepApplication.java
@@ -0,0 +1,13 @@
+package com.practice.sadeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SadeepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SadeepApplication.class, args);
+	}
+
+}
