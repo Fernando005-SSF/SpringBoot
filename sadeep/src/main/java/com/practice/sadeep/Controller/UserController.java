4071d87e96d768293ed66f4d607fea66ba15a193
Fernando005-SSF <sadeepfernando12345@gmail.com>
2024-03-04 08:19:45 +0530

    Created the end point to get and send user from the database

--- sadeep/src/main/java/com/practice/sadeep/Controller/UserController.java
+++ sadeep/src/main/java/com/practice/sadeep/Controller/UserController.java
@@ -3,20 +3,29 @@ package com.practice.sadeep.Controller;
 import com.practice.sadeep.Model.User;
 import com.practice.sadeep.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 
 @RestController
-public class UserController extends User {
+public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
     @PostMapping("/user")
     User newUser(@RequestBody User newUser){
+
         return userRepository.save(newUser);
+    }
+
+    @GetMapping("/user")
+    List<User> getUsers(){
+        return userRepository.findAll();
     }
 
 }
